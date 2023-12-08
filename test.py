59def604d6b17e46a29e5a1561a0766d7ce7eaff
Rob Madden <rmadden@chicagofirefc.com>
2023-12-08 14:03:02 -0600

    Update to be more module friendly

---
+++ test.py
@@ -0,0 +1,11 @@
+import logging
+
+from loggerhead import LoggerHead
+
+LoggerHead()
+
+logging.critical("CRITICAL")
+logging.error("ERROR")
+logging.warning("WARNING")
+logging.debug("DEBUG")
+logging.info("INFO")
